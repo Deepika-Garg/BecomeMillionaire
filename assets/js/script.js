b9be29f89e6bad3555b10827650cb54991c1b137
Deepika-Garg <garg.deepika21@gmail.com>
2025-04-18 15:13:46 +0100

    add game image, html, css and js file. added bootstrap container to devide screen. added 1 question and 4 answers button. added essential css style for them.

---
+++ assets/js/script.js
@@ -0,0 +1,136 @@
+const q1set = [
+    {
+        question: "What is the name of the ancient Incan city in Peru that is considered one of the new Seven Wonders of the World?",
+        a: "Angkor Wat",
+        b: "Petra",
+        c: "Machu Picchu",
+        d: "The Colosseum",
+        correct: "Machu Picchu"
+    },
+    {
+        question: "What is the name of the famous tower in Italy that leans to one side?",
+        a: "Eiffel Tower",
+        b: "CN Tower",
+        c: "Leaning Tower of Pisa",
+        d: "Tokyo Tower",
+        correct: "Leaning Tower of Pisa"
+    },
+    {
+        question: "What is the name of the famous monument in the USA that was built to commemorate the first US President?",
+        a: "Mount Rushmore",
+        b: "Lincoln Memorial",
+        c: "Statue of Liberty",
+        d: "Washington Monument",
+        correct: "Washington Monument"
+    }
+]
+
+const q2set = [
+    {
+        question: "What is the largest continent in the world?",
+        a: "Africa",
+        b: "Asia",
+        c: "North America",
+        d: "South America",
+        correct: "Asia"
+    },
+    {
+        question: "What is the smallest continent in the world?",
+        a: "Antarctica",
+        b: "Europe",
+        c: "Australia",
+        d: "South America",
+        correct: "Australia"
+    },
+    {
+        question: "What is the only continent that is also a country?",
+        a: "Europe",
+        b: "Africa",
+        c: "Australia",
+        d: "North America",
+        correct: "Australia"
+    }
+];
+
+
+const q3set = [
+    {
+        question: "What is the largest ocean in the world?",
+        a: "Atlantic Ocean",
+        b: "Arctic Ocean",
+        c: "Indian Ocean",
+        d: "Pacific Ocean",
+        correct: "Pacific Ocean"
+    },
+    {
+        question: "What is the smallest ocean in the world?",
+        a: "Arctic Ocean",
+        b: "Southern Ocean",
+        c: "Indian Ocean",
+        d: "Atlantic Ocean",
+        correct: "Southern Ocean"
+    },
+    {
+        question: "What is the deepest ocean in the world?",
+        a: "Atlantic Ocean",
+        b: "Arctic Ocean",
+        c: "Indian Ocean",
+        d: "Pacific Ocean",
+        correct: "Pacific Ocean"
+    }
+];
+
+const q4set = [
+    {
+        question: "What is the largest desert in the world?",
+        a: "Gobi Desert",
+        b: "Sahara Desert",
+        c: "Mojave Desert",
+        d: "Kalahari Desert",
+        correct: "Sahara Desert"
+    },
+    {
+        question: "What is the only desert in the world that is home to a large number of wild camels?",
+        a: "Mojave Desert",
+        b: "Sahara Desert",
+        c: "Gobi Desert",
+        d: "Arabian Desert",
+        correct: "Gobi Desert"
+    },
+    {
+        question: "What is the largest hot desert in the world?",
+        a: "Sahara Desert",
+        b: "Mojave Desert",
+
+    }
+]
+
+const q5set = [
+    {
+        question: "What is the capital of Japan?",
+        a: "Tokyo",
+        b: "Osaka",
+        c: "Kyoto",
+        d: "Hiroshima",
+        correct: "Tokyo",
+
+    },
+    {
+        question: "What is the largest country in South America?",
+        a: "Argentina",
+        b: "Peru",
+        c: "Brazil",
+        d: "Colombia",
+        correct: "Brazil",
+
+    },
+    {
+        question: "What is the deepest point in the ocean?",
+        a: "Mariana Trench",
+        b: "Puerto Rico Trench",
+        c: "Java Trench",
+        d: "Aleutian Trench",
+        correct: "Mariana Trench",
+
+    },
+];
