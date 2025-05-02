aaaaad1473bb1321ff552da88a566296fb612947
Deepika-Garg <garg.deepika21@gmail.com>
2025-05-02 14:58:57 +0100

    add logic for showing que and ans in the container choose randomly from predeclared sets.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,380 +1,433 @@
 const q1set = [
-    {
-        question: "What is the name of the ancient Incan city in Peru that is considered one of the new Seven Wonders of the World?",
-        a: "Angkor Wat",
-        b: "Petra",
-        c: "Machu Picchu",
-        d: "The Colosseum",
-        correct: "Machu Picchu"
-    },
-    {
-        question: "What is the name of the famous tower in Italy that leans to one side?",
-        a: "Eiffel Tower",
-        b: "CN Tower",
-        c: "Leaning Tower of Pisa",
-        d: "Tokyo Tower",
-        correct: "Leaning Tower of Pisa"
-    },
-    {
-        question: "What is the name of the famous monument in the USA that was built to commemorate the first US President?",
-        a: "Mount Rushmore",
-        b: "Lincoln Memorial",
-        c: "Statue of Liberty",
-        d: "Washington Monument",
-        correct: "Washington Monument"
-    }
-]
-
-const q2set = [
-    {
-        question: "What is the largest continent in the world?",
-        a: "Africa",
-        b: "Asia",
-        c: "North America",
-        d: "South America",
-        correct: "Asia"
-    },
-    {
-        question: "What is the smallest continent in the world?",
-        a: "Antarctica",
-        b: "Europe",
-        c: "Australia",
-        d: "South America",
-        correct: "Australia"
-    },
-    {
-        question: "What is the only continent that is also a country?",
-        a: "Europe",
-        b: "Africa",
-        c: "Australia",
-        d: "North America",
-        correct: "Australia"
-    }
+  {
+    question:
+      "What is the name of the ancient Incan city in Peru that is considered one of the new Seven Wonders of the World?",
+    a: "Angkor Wat",
+    b: "Petra",
+    c: "Machu Picchu",
+    d: "The Colosseum",
+    correct: "Machu Picchu",
+  },
+  {
+    question:
+      "What is the name of the famous tower in Italy that leans to one side?",
+    a: "Eiffel Tower",
+    b: "CN Tower",
+    c: "Leaning Tower of Pisa",
+    d: "Tokyo Tower",
+    correct: "Leaning Tower of Pisa",
+  },
+  {
+    question:
+      "What is the name of the famous monument in the USA that was built to commemorate the first US President?",
+    a: "Mount Rushmore",
+    b: "Lincoln Memorial",
+    c: "Statue of Liberty",
+    d: "Washington Monument",
+    correct: "Washington Monument",
+  },
 ];
 
+const q2set = [
+  {
+    question: "What is the largest continent in the world?",
+    a: "Africa",
+    b: "Asia",
+    c: "North America",
+    d: "South America",
+    correct: "Asia",
+  },
+  {
+    question: "What is the smallest continent in the world?",
+    a: "Antarctica",
+    b: "Europe",
+    c: "Australia",
+    d: "South America",
+    correct: "Australia",
+  },
+  {
+    question: "What is the only continent that is also a country?",
+    a: "Europe",
+    b: "Africa",
+    c: "Australia",
+    d: "North America",
+    correct: "Australia",
+  },
+];
 
 const q3set = [
-    {
-        question: "What is the largest ocean in the world?",
-        a: "Atlantic Ocean",
-        b: "Arctic Ocean",
-        c: "Indian Ocean",
-        d: "Pacific Ocean",
-        correct: "Pacific Ocean"
-    },
-    {
-        question: "What is the smallest ocean in the world?",
-        a: "Arctic Ocean",
-        b: "Southern Ocean",
-        c: "Indian Ocean",
-        d: "Atlantic Ocean",
-        correct: "Southern Ocean"
-    },
-    {
-        question: "What is the deepest ocean in the world?",
-        a: "Atlantic Ocean",
-        b: "Arctic Ocean",
-        c: "Indian Ocean",
-        d: "Pacific Ocean",
-        correct: "Pacific Ocean"
-    }
+  {
+    question: "What is the largest ocean in the world?",
+    a: "Atlantic Ocean",
+    b: "Arctic Ocean",
+    c: "Indian Ocean",
+    d: "Pacific Ocean",
+    correct: "Pacific Ocean",
+  },
+  {
+    question: "What is the smallest ocean in the world?",
+    a: "Arctic Ocean",
+    b: "Southern Ocean",
+    c: "Indian Ocean",
+    d: "Atlantic Ocean",
+    correct: "Southern Ocean",
+  },
+  {
+    question: "What is the deepest ocean in the world?",
+    a: "Atlantic Ocean",
+    b: "Arctic Ocean",
+    c: "Indian Ocean",
+    d: "Pacific Ocean",
+    correct: "Pacific Ocean",
+  },
 ];
 
 const q4set = [
-    {
-        question: "What is the largest desert in the world?",
-        a: "Gobi Desert",
-        b: "Sahara Desert",
-        c: "Mojave Desert",
-        d: "Kalahari Desert",
-        correct: "Sahara Desert"
-    },
-    {
-        question: "What is the only desert in the world that is home to a large number of wild camels?",
-        a: "Mojave Desert",
-        b: "Sahara Desert",
-        c: "Gobi Desert",
-        d: "Arabian Desert",
-        correct: "Gobi Desert"
-    },
-    {
-        question: "What is the largest hot desert in the world?",
-        a: "Sahara Desert",
-        b: "Mojave Desert",
-
-    }
-]
+  {
+    question: "What is the largest desert in the world?",
+    a: "Gobi Desert",
+    b: "Sahara Desert",
+    c: "Mojave Desert",
+    d: "Kalahari Desert",
+    correct: "Sahara Desert",
+  },
+  {
+    question:
+      "What is the only desert in the world that is home to a large number of wild camels?",
+    a: "Mojave Desert",
+    b: "Sahara Desert",
+    c: "Gobi Desert",
+    d: "Arabian Desert",
+    correct: "Gobi Desert",
+  },
+  {
+    question: "What is the largest hot desert in the world?",
+    a: "Sahara Desert",
+    b: "Mojave Desert",
+    c: "Gobi Desert",
+    d: "Kalahari Desert",
+    correct: "Sahara Desert",
+  },
+];
 
 const q5set = [
-    {
-        question: "What is the capital of Japan?",
-        a: "Tokyo",
-        b: "Osaka",
-        c: "Kyoto",
-        d: "Hiroshima",
-        correct: "Tokyo",
-
-    },
-    {
-        question: "What is the largest country in South America?",
-        a: "Argentina",
-        b: "Peru",
-        c: "Brazil",
-        d: "Colombia",
-        correct: "Brazil",
-
-    },
-    {
-        question: "What is the deepest point in the ocean?",
-        a: "Mariana Trench",
-        b: "Puerto Rico Trench",
-        c: "Java Trench",
-        d: "Aleutian Trench",
-        correct: "Mariana Trench",
-
-    },
-
+  {
+    question: "What is the capital of Japan?",
+    a: "Tokyo",
+    b: "Osaka",
+    c: "Kyoto",
+    d: "Hiroshima",
+    correct: "Tokyo",
+  },
+  {
+    question: "What is the largest country in South America?",
+    a: "Argentina",
+    b: "Peru",
+    c: "Brazil",
+    d: "Colombia",
+    correct: "Brazil",
+  },
+  {
+    question: "What is the deepest point in the ocean?",
+    a: "Mariana Trench",
+    b: "Puerto Rico Trench",
+    c: "Java Trench",
+    d: "Aleutian Trench",
+    correct: "Mariana Trench",
+  },
 ];
 const q6set = [
-    {
-        question: "What is the largest desert in the world?",
-        a: "Sahara Desert",
-        b: "Arabian Desert",
-        c: "Gobi Desert",
-        d: "Antarctica",
-        correct: "Antarctica",
-
-    },
-    {
-        question: "What is the capital of Canada?",
-        a: "Toronto",
-        b: "Montreal",
-        c: "Ottawa",
-        d: "Vancouver",
-        correct: "Ottawa",
-
-    },
-    {
-        question: "What is the smallest continent in the world?",
-        a: "Africa",
-        b: "Europe",
-        c: "Asia",
-        d: "Australia",
-        correct: "Australia",
-
-    },
+  {
+    question: "What is the largest desert in the world?",
+    a: "Sahara Desert",
+    b: "Arabian Desert",
+    c: "Gobi Desert",
+    d: "Antarctica",
+    correct: "Antarctica",
+  },
+  {
+    question: "What is the capital of Canada?",
+    a: "Toronto",
+    b: "Montreal",
+    c: "Ottawa",
+    d: "Vancouver",
+    correct: "Ottawa",
+  },
+  {
+    question: "What is the smallest continent in the world?",
+    a: "Africa",
+    b: "Europe",
+    c: "Asia",
+    d: "Australia",
+    correct: "Australia",
+  },
 ];
 
 const q7set = [
-    {
-        question: "What is the highest waterfall in the world?",
-        a: "Angel Falls",
-        b: "Niagara Falls",
-        c: "Iguazu Falls",
-        d: "Victoria Falls",
-        correct: "Angel Falls",
-
-    },
-    {
-        question: "What is the capital of Italy?",
-        a: "Rome",
-        b: "Venice",
-        c: "Milan",
-        d: "Florence",
-        correct: "Rome",
-
-    },
-    {
-        question: "What is the largest lake in Africa?",
-        a: "Lake Tanganyika",
-        b: "Lake Malawi",
-        c: "Lake Victoria",
-        d: "Lake Turkana",
-        correct: "Lake Victoria",
-
-    },
+  {
+    question: "What is the highest waterfall in the world?",
+    a: "Angel Falls",
+    b: "Niagara Falls",
+    c: "Iguazu Falls",
+    d: "Victoria Falls",
+    correct: "Angel Falls",
+  },
+  {
+    question: "What is the capital of Italy?",
+    a: "Rome",
+    b: "Venice",
+    c: "Milan",
+    d: "Florence",
+    correct: "Rome",
+  },
+  {
+    question: "What is the largest lake in Africa?",
+    a: "Lake Tanganyika",
+    b: "Lake Malawi",
+    c: "Lake Victoria",
+    d: "Lake Turkana",
+    correct: "Lake Victoria",
+  },
 ];
 
 const q8set = [
-    {
-        question: "What is the tallest animal in the world?",
-        a: "Hippopotamus",
-        b: "Giraffe",
-        c: "Elephant",
-        d: "Rhino",
-        correct: "Giraffe",
-    },
-    {
-        question: "What is the capital of Brazil?",
-        a: "Rio de Janeiro",
-        b: "Brasília",
-        c: "São Paulo",
-        d: "Belo Horizonte",
-        correct: "Brasília",
-    },
-    {
-        question: "What is the largest country in the world?",
-        a: "Russia",
-        b: "Canada",
-        c: "China",
-        d: "United States",
-        correct: "Russia",
-    },
+  {
+    question: "What is the tallest animal in the world?",
+    a: "Hippopotamus",
+    b: "Giraffe",
+    c: "Elephant",
+    d: "Rhino",
+    correct: "Giraffe",
+  },
+  {
+    question: "What is the capital of Brazil?",
+    a: "Rio de Janeiro",
+    b: "Brasília",
+    c: "São Paulo",
+    d: "Belo Horizonte",
+    correct: "Brasília",
+  },
+  {
+    question: "What is the largest country in the world?",
+    a: "Russia",
+    b: "Canada",
+    c: "China",
+    d: "United States",
+    correct: "Russia",
+  },
 ];
 
 const q9set = [
-    {
-        question: "What is the name of the ancient Incan city in Peru that is considered one of the new Seven Wonders of the World?",
-        a: "Angkor Wat",
-        b: "Petra",
-        c: "Machu Picchu",
-        d: "The Colosseum",
-        correct: "Machu Picchu"
-    },
-    {
-        question: "What is the name of the famous tower in Italy that leans to one side?",
-        a: "Eiffel Tower",
-        b: "CN Tower",
-        c: "Leaning Tower of Pisa",
-        d: "Tokyo Tower",
-        correct: "Leaning Tower of Pisa"
-    },
-    {
-        question: "What is the name of the famous monument in the USA that was built to commemorate the first US President?",
-        a: "Mount Rushmore",
-        b: "Lincoln Memorial",
-        c: "Statue of Liberty",
-        d: "Washington Monument",
-        correct: "Washington Monument"
-    }
+  {
+    question:
+      "What is the name of the ancient Incan city in Peru that is considered one of the new Seven Wonders of the World?",
+    a: "Angkor Wat",
+    b: "Petra",
+    c: "Machu Picchu",
+    d: "The Colosseum",
+    correct: "Machu Picchu",
+  },
+  {
+    question:
+      "What is the name of the famous tower in Italy that leans to one side?",
+    a: "Eiffel Tower",
+    b: "CN Tower",
+    c: "Leaning Tower of Pisa",
+    d: "Tokyo Tower",
+    correct: "Leaning Tower of Pisa",
+  },
+  {
+    question:
+      "What is the name of the famous monument in the USA that was built to commemorate the first US President?",
+    a: "Mount Rushmore",
+    b: "Lincoln Memorial",
+    c: "Statue of Liberty",
+    d: "Washington Monument",
+    correct: "Washington Monument",
+  },
 ];
 
 const q10set = [
-    {
-        question: "What is the largest continent in the world?",
-        a: "Africa",
-        b: "Asia",
-        c: "North America",
-        d: "South America",
-        correct: "Asia"
-    },
-    {
-        question: "What is the smallest continent in the world?",
-        a: "Antarctica",
-        b: "Europe",
-        c: "Australia",
-        d: "South America",
-        correct: "Australia"
-    },
-    {
-        question: "What is the only continent that is also a country?",
-        a: "Europe",
-        b: "Africa",
-        c: "Australia",
-        d: "North America",
-        correct: "Australia"
-    }
+  {
+    question: "What is the largest continent in the world?",
+    a: "Africa",
+    b: "Asia",
+    c: "North America",
+    d: "South America",
+    correct: "Asia",
+  },
+  {
+    question: "What is the smallest continent in the world?",
+    a: "Antarctica",
+    b: "Europe",
+    c: "Australia",
+    d: "South America",
+    correct: "Australia",
+  },
+  {
+    question: "What is the only continent that is also a country?",
+    a: "Europe",
+    b: "Africa",
+    c: "Australia",
+    d: "North America",
+    correct: "Australia",
+  },
 ];
 
 const q11set = [
-    {
-        question: "What is the largest ocean in the world?",
-        a: "Atlantic Ocean",
-        b: "Arctic Ocean",
-        c: "Indian Ocean",
-        d: "Pacific Ocean",
-        correct: "Pacific Ocean"
-    },
-    {
-        question: "What is the smallest ocean in the world?",
-        a: "Arctic Ocean",
-        b: "Southern Ocean",
-        c: "Indian Ocean",
-        d: "Atlantic Ocean",
-        correct: "Southern Ocean"
-    },
-    {
-        question: "What is the deepest ocean in the world?",
-        a: "Atlantic Ocean",
-        b: "Arctic Ocean",
-        c: "Indian Ocean",
-        d: "Pacific Ocean",
-        correct: "Pacific Ocean"
-    }
+  {
+    question: "What is the largest ocean in the world?",
+    a: "Atlantic Ocean",
+    b: "Arctic Ocean",
+    c: "Indian Ocean",
+    d: "Pacific Ocean",
+    correct: "Pacific Ocean",
+  },
+  {
+    question: "What is the smallest ocean in the world?",
+    a: "Arctic Ocean",
+    b: "Southern Ocean",
+    c: "Indian Ocean",
+    d: "Atlantic Ocean",
+    correct: "Southern Ocean",
+  },
+  {
+    question: "What is the deepest ocean in the world?",
+    a: "Atlantic Ocean",
+    b: "Arctic Ocean",
+    c: "Indian Ocean",
+    d: "Pacific Ocean",
+    correct: "Pacific Ocean",
+  },
 ];
 
 const q12set = [
-    {
-        question: "What is the largest desert in the world?",
-        a: "Gobi Desert",
-        b: "Sahara Desert",
-        c: "Mojave Desert",
-        d: "Kalahari Desert",
-        correct: "Sahara Desert"
-    },
-    {
-        question: "What is the only desert in the world that is home to a large number of wild camels?",
-        a: "Mojave Desert",
-        b: "Sahara Desert",
-        c: "Gobi Desert",
-        d: "Arabian Desert",
-        correct: "Gobi Desert"
-    },
-    {
-        question: "What is the largest hot desert in the world?",
-        a: "Sahara Desert",
-        b: "Mojave Desert",
+  {
+    question: "What is the largest desert in the world?",
+    a: "Gobi Desert",
+    b: "Sahara Desert",
+    c: "Mojave Desert",
+    d: "Kalahari Desert",
+    correct: "Sahara Desert",
+  },
+  {
+    question:
+      "What is the only desert in the world that is home to a large number of wild camels?",
+    a: "Mojave Desert",
+    b: "Sahara Desert",
+    c: "Gobi Desert",
+    d: "Arabian Desert",
+    correct: "Gobi Desert",
+  },
+  {
+    question: "What is the largest hot desert in the world?",
+    a: "Sahara Desert",
+    b: "Mojave Desert",
+  },
+];
 
-    }
-]
-;
+/*-- Define progressSet array with 12 ids and their corresponding prize values sets --*/ 
 const progressSet = [
-    {
-        id: 1,
-        prize: 100,
-    },
-    {
-        id: 2,
-        prize: 500,
-    },
-    {
-        id: 3,
-        prize: 1000,
-    },
-    {
-        id: 4,
-        prize: 4000,
-    },
-    {
-        id: 5,
-        prize: 8000,
-    },
-    {
-        id: 6,
-        prize: 16000,
-    },
-    {
-        id: 7,
-        prize: 32000,
-    },
-    {
-        id: 8,
-        prize: 64000,
-    },
-    {
-        id: 9,
-        prize: 125000,
-    },
-    {
-        id: 10,
-        prize: 250000,
-    },
-    {
-        id: 11,
-        prize: 500000,
-    },
-    {
-        id: 12,
-        prize: 1000000,
-    },
-]
+  {
+    id: 1,
+    prize: 100,
+  },
+  {
+    id: 2,
+    prize: 500,
+  },
+  {
+    id: 3,
+    prize: 1000,
+  },
+  {
+    id: 4,
+    prize: 4000,
+  },
+  {
+    id: 5,
+    prize: 8000,
+  },
+  {
+    id: 6,
+    prize: 16000,
+  },
+  {
+    id: 7,
+    prize: 32000,
+  },
+  {
+    id: 8,
+    prize: 64000,
+  },
+  {
+    id: 9,
+    prize: 125000,
+  },
+  {
+    id: 10,
+    prize: 250000,
+  },
+  {
+    id: 11,
+    prize: 500000,
+  },
+  {
+    id: 12,
+    prize: 1000000,
+  },
+];
+
+const questionSets = {
+    1: q1set,
+    2: q2set,
+    3: q3set,
+    4: q4set,
+    5: q5set,
+    6: q6set,
+    7: q7set,
+    8: q8set,
+    9: q9set,
+    10: q10set,
+    11: q11set,
+    12: q12set,
+  };
+let currentQuestion = 1;
+let score = 0;
+
+const question = document.querySelector(".question div p");
+const allAnswers = document.querySelector(".answer");
+
+//set random question and their answer from the given set to show on screen
+function setQueAndAns(queSet) {
+   
+  let random = Math.floor(Math.random() * queSet.length);
+  let randomquestion = queSet[random];
+  question.textContent = randomquestion.question;
+  allAnswers.innerHTML=`<div class="row">
+                <div class="col-12 col-md-6 mb-2">
+                  <button type="button" class="btn w-100"><span>A. </span>${randomquestion.a}</button>
+                </div>
+                <div class="col-12 col-md-6 mb-2">
+                  <button type="button" class="btn w-100"><span>B. </span>${randomquestion.b}</button>
+                </div>
+              </div>
+              <div class="row ">
+                <div class="col-12 col-md-6 mb-2">
+                  <button type="button" class="btn w-100"><span>C. </span>${randomquestion.c}</button>
+                </div>
+                <div class="col-12 col-md-6 mb-2">
+                  <button type="button" class="btn w-100"><span>D. </span>${randomquestion.d}</button>
+                </div>
+              </div>`
+
+}
+
+
+const showQueAndAns = (currentQuestion) => {
+  
+      setQueAndAns(questionSets[currentQuestion]);
+  
+};
+showQueAndAns(currentQuestion);
+
